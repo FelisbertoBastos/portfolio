b401ba1020d8495c1030f8b5aa11968e604be412
Felisberto Bastos <felisbertobastos22@gmail.com>
2019-01-11 08:08:05 -0300

    Componente FlexItem criado

---
+++ src/components/modules/FlexItem.vue
@@ -0,0 +1,47 @@
+<template>
+  <div
+    class="flex-item"
+    :class="classArray"
+    :style="styleObject"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FlexItem',
+  props: {
+    order: {
+      type: Number
+    },
+    flexGrow: {
+      type: Number
+    },
+    flexShrink: {
+      type: Number
+    },
+    flexBasis: {
+      type: String
+    },
+    alignSelf: {
+      type: String
+    }
+  },
+  computed: {
+    classArray () {
+      return [
+        `align-self-${this.alignSelf}`
+      ]
+    },
+    styleObject () {
+      return {
+        order: this.order,
+        flexGrow: this.flexGrow,
+        flexShrink: this.flexShrink,
+        flexBasis: this.flexBasis
+      }
+    }
+  }
+}
+</script>
