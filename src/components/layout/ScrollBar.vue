ef59d1fffe48f853310bf4208d6ef93c89ec3c18
Felisberto Bastos <felisbertobastos22@gmail.com>
2019-01-24 10:25:34 -0300

    ScrollBar com steps

--- src/components/layout/ScrollBar.vue
+++ src/components/layout/ScrollBar.vue
@@ -1,13 +1,94 @@
 <template>
-  <div class="scroll-bar">
+  <div id="scroll-bar" class="scroll-bar">
     <div class="scroll-path"></div>
-    <div class="scroll-control"></div>
+    <div id="scroll-control" class="scroll-control"></div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'ScrollBar'
+  name: 'ScrollBar',
+  mounted () {
+    var scrollControl = document.getElementById('scroll-control')
+    var scrollBar = document.getElementById('scroll-bar')
+
+    const steps = 3
+    const min = 0
+    const max = 200 - 60
+    const stepsArray = []
+    for (var i = 0; i < steps - 1; i++) {
+      stepsArray.push(Math.round(i * max / (steps - 1)))
+    }
+    stepsArray.push(max)
+
+    var active = false
+    var currentY = 0
+    var initialY = 0
+    var yOffset = 0
+
+    scrollBar.addEventListener('touchstart', dragStart, false)
+    scrollBar.addEventListener('touchend', dragEnd, false)
+    scrollBar.addEventListener('touchmove', drag, false)
+
+    scrollBar.addEventListener('mousedown', dragStart, false)
+    scrollBar.addEventListener('mouseup', dragEnd, false)
+    scrollBar.addEventListener('mousemove', drag, false)
+
+    function dragStart (e) {
+      if (e.type === 'touchstart') {
+        initialY = e.touches[0].clientY - yOffset
+      } else {
+        initialY = e.clientY - yOffset
+      }
+
+      if (e.target === scrollControl) {
+        scrollControl.classList.remove('scroll-transition')
+        active = true
+      }
+    }
+
+    function dragEnd (e) {
+      initialY = currentY
+
+      active = false
+
+      var closest = Number.MAX_SAFE_INTEGER
+      var index = 0
+      stepsArray.forEach((num, i) => {
+        var dist = Math.abs(currentY - num)
+        if (dist < closest) {
+          index = i
+          closest = dist
+        }
+      })
+      yOffset = stepsArray[index]
+
+      scrollControl.classList.add('scroll-transition')
+      setTranslate(yOffset, scrollControl)
+    }
+
+    function drag (e) {
+      if (active) {
+        e.preventDefault()
+
+        if (e.type === 'touchmove') {
+          currentY = e.touches[0].clientY - initialY
+        } else {
+          currentY = e.clientY - initialY
+        }
+
+        yOffset = currentY
+
+        if (currentY >= min && currentY <= max) {
+          setTranslate(currentY, scrollControl)
+        }
+      }
+    }
+
+    function setTranslate (yPos, el) {
+      el.style.transform = 'translate3d(0, ' + yPos + 'px, 0)'
+    }
+  }
 }
 </script>
 
@@ -33,4 +114,7 @@ export default {
     border: 2px solid #707070
     border-radius: 50% / 10px
     box-shadow: 0 0 9px rgba(0, 0, 0, 0.9)
+
+  .scroll-transition
+    transition: transform 0.5s
 </style>
